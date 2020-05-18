1ec6600057a2eb4b3f5c774f238b9afde5958796
Brittany Fisher-Morris <bmmf@bms-imac.home>
2020-05-17 22:02:44 -0400

    elements created and structured for header component in index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,22 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+// add Header to its parent so it can display
+
+function Header() {
+    // creating elements
+    const headerTop = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    // creating structure (attaching to parentals)
+    headerTop.appendChild(date);
+    headerTop.appendChild(title);
+    headerTop.appendChild(temp);
+
+    // adding classes
+
+
+    // adding text
+}
